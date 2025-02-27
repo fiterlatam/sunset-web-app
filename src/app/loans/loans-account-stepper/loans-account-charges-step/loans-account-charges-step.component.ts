3481cdbdcbfa7015b4c68d043180cde5dcdd22fd
Yeison Noreña Osorio <ynorena@fgs.co>
2025-02-27 16:42:22 +0000

    Merged PR 8364: release/v1.0.10-dev-prepare
    Related work items: #206, #210, #212, #213, #215, #216, #217, #218, #219, #221, #223, #224, #226, #227, #228, #229, #230, #231, #232, #233, #234, #235, #236, #237, #238, #239, #240, #241, #242, #243, #244, #245, #246, #247, #248, #249, #250, #253, #254, #257, #258, #259, #260, #261, #262, #263, #264, #265, #266, #267, #268, #269, #270, #271, #274, #275, #276, #277, #279, #280, #282, #283, #284, #285, #286, #287, #288, #289, #291, #302, #303, #30599

--- src/app/loans/loans-account-stepper/loans-account-charges-step/loans-account-charges-step.component.ts
+++ src/app/loans/loans-account-stepper/loans-account-charges-step/loans-account-charges-step.component.ts
@@ -1,25 +1,23 @@
 /** Angular Imports */
-import { Component, ViewChild, ElementRef, OnInit, Input, OnChanges } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
+import {Component, ViewChild, OnInit, Input, OnChanges} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
 
 /** Dialog Components */
-import { DeleteDialogComponent } from 'app/shared/delete-dialog/delete-dialog.component';
-import { FormDialogComponent } from 'app/shared/form-dialog/form-dialog.component';
-import { LoansAccountAddCollateralDialogComponent } from 'app/loans/custom-dialog/loans-account-add-collateral-dialog/loans-account-add-collateral-dialog.component';
+import {DeleteDialogComponent} from 'app/shared/delete-dialog/delete-dialog.component';
+import {FormDialogComponent} from 'app/shared/form-dialog/form-dialog.component';
 
 /** Custom Services */
-import { DatepickerBase } from 'app/shared/form-dialog/formfield/model/datepicker-base';
-import { FormfieldBase } from 'app/shared/form-dialog/formfield/model/formfield-base';
-import { InputBase } from 'app/shared/form-dialog/formfield/model/input-base';
-import { SettingsService } from 'app/settings/settings.service';
-import { Dates } from 'app/core/utils/dates';
-import { ActivatedRoute } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
+import {DatepickerBase} from 'app/shared/form-dialog/formfield/model/datepicker-base';
+import {FormfieldBase} from 'app/shared/form-dialog/formfield/model/formfield-base';
+import {InputBase} from 'app/shared/form-dialog/formfield/model/input-base';
+import {SettingsService} from 'app/settings/settings.service';
+import {Dates} from 'app/core/utils/dates';
+import {ActivatedRoute} from '@angular/router';
+import {TranslateService} from '@ngx-translate/core';
 
-import { principalAmountChangeEvent } from '../loans-account-terms-step/loans-account-terms-step.component'; // Importe o EventEmitter do ComponentB
-import { MatSelect } from '@angular/material/select';
-import { Charge } from 'app/shared/models/general.model';
+import {principalAmountChangeEvent} from '../loans-account-terms-step/loans-account-terms-step.component'; // Importe o EventEmitter do ComponentB
+import {MatSelect} from '@angular/material/select';
+import {Charge} from 'app/shared/models/general.model';
 
 /**
  * Recurring Deposit Account Charges Step
@@ -51,7 +49,7 @@ export class LoansAccountChargesStepComponent implements OnInit, OnChanges {
   /** Collateral Data Source */
   collateralDataSource: {}[] = [];
   /** Charges table columns */
-  chargesDisplayedColumns: string[] = ['name', 'chargeCalculationType', 'amount', 'chargeTimeType', 'date', 'action','endorsed'];
+  chargesDisplayedColumns: string[] = ['name', 'chargeCalculationType', 'amount', 'chargeTimeType', 'date', 'action', 'endorsed'];
   /** Columns to be displayed in overdue charges table. */
   overdueChargesDisplayedColumns: string[] = ['name', 'type', 'amount', 'collectedon'];
   /** Component is pristine if there has been no changes by user interaction */
@@ -62,90 +60,99 @@ export class LoansAccountChargesStepComponent implements OnInit, OnChanges {
   totalCollateralValue: any = 0;
   loanId: any = null;
   /** Check if the product is Vehiculos */
-  isVehiculos: boolean = false;
+  isVehiculos = false;
   /** Maximum date allowed. */
   maxDate = new Date(2100, 0, 1);
 
   /**
    * Loans Account Charges Form Step
-   * @param {dialog} MatDialog Mat Dialog
+   * @param dialog
    * @param {Dates} dateUtils Date Utils
+   * @param route
    * @param {SettingsService} settingsService Settings Service
+   * @param translate
    */
   constructor(public dialog: MatDialog,
-    private dateUtils: Dates,
-    private route: ActivatedRoute,
-    private settingsService: SettingsService, private translate: TranslateService) {
+              private dateUtils: Dates,
+              private route: ActivatedRoute,
+              private settingsService: SettingsService, private translate: TranslateService) {
     this.loanId = this.route.snapshot.params['loanId'];
   }
 
   ngOnInit() {
     this.maxDate = this.settingsService.maxFutureDate;
     if (this.loansAccountTemplate && this.loansAccountTemplate.charges) {
-      if(this.loansAccountProductTemplate?.product?.productType?.name === "SU+ Vehiculos"){
+      if (this.loansAccountProductTemplate?.product?.productType?.name === 'SU+ Vehiculos') {
         this.isVehiculos = true;
-        this.chargesDisplayedColumns= ['name', 'chargeCalculationType', 'amount', 'chargeTimeType', 'action', 'endorsed'];
+        this.chargesDisplayedColumns = ['name', 'chargeCalculationType', 'amount', 'chargeTimeType', 'action', 'endorsed'];
       }
-      this.chargesDataSource = this.loansAccountTemplate.charges.map((charge: any) => ({ ...charge, id: charge.chargeId, expdate: charge?.expDate || null, isEndorsed: charge.amount === 0, insuranceName: charge?.insuranceName, insuranceId : charge?.insuranceId })) || [];
+      this.chargesDataSource = this.loansAccountTemplate.charges.map((charge: any) => ({
+        ...charge,
+        id: charge.chargeId,
+        expdate: charge?.expDate || null,
+        isEndorsed: charge.amount === 0,
+        insuranceName: charge?.insuranceName,
+        insuranceId: charge?.insuranceId
+      })) || [];
     }
 
     principalAmountChangeEvent.subscribe((newPrincipalAmountValue: any) => {
 
       // Delete all "Comision Mi Pyme" charge previously associated to this loan
       this.chargesDataSource = this.chargesDataSource.filter(
-        (item: Charge) => !item.name?.toLowerCase().includes("comision mi pyme") && !item.name?.toLowerCase().includes("capital pendiente mi pyme")
+        (item: Charge) => !item.name?.toLowerCase().includes('comision mi pyme') && !item.name?.toLowerCase().includes('capital pendiente mi pyme')
       );
 
 
       // Filter charges by amount and associate them out
       if (this.loansAccountProductTemplate.loanProductName.toLowerCase().includes('microcredito')) {
 
-        var amountLimit = this.loansAccountProductTemplate.smvl;
+        const amountLimit = 4 * this.loansAccountProductTemplate.smvl;
         let informationIndex = -1;
 
-        let chargeRootName = "Capital Pendiente";
+        let chargeRootName = 'Capital Pendiente';
 
-        if (this.loansAccountProductTemplate.loanProductName.toLowerCase() == 'microcredito' 
-              || this.loansAccountProductTemplate.loanProductName.toLowerCase() == 'microcredito m' ) {
-          chargeRootName = "Comision";
+        if (this.loansAccountProductTemplate.loanProductName.toLowerCase() === 'microcredito'
+          || this.loansAccountProductTemplate.loanProductName.toLowerCase() === 'microcredito m') {
+          chargeRootName = 'Comision';
         }
 
-        var filterCriteria = chargeRootName + " Mi Pyme >= 4SMLV";
+        let filterCriteria = chargeRootName + ' Mi Pyme >= 4SMLV';
 
-        if(newPrincipalAmountValue.id < amountLimit) {
-          filterCriteria = chargeRootName + " Mi Pyme < 4SMLV";
+        if (newPrincipalAmountValue.id < amountLimit) {
+          filterCriteria = chargeRootName + ' Mi Pyme < 4SMLV';
         }
-        
+
 
         // Filter charges by name to correspond to the filterCriteria, depending on the amount.
         this.chargeData.forEach((item: Charge) => {
 
           informationIndex++;
 
-          if(item.name?.toLowerCase().includes(filterCriteria.toLowerCase())) {
+          if (item.name?.toLowerCase().includes(filterCriteria.toLowerCase())) {
 
             const mappedItem = {
-              chargeId:       this.chargeData[informationIndex].id,
-              name:       this.chargeData[informationIndex].name,
-              chargeTimeType:       this.chargeData[informationIndex].chargeTimeType,
-              chargeCalculationType:       this.chargeData[informationIndex].chargeCalculationType,
-              currency:       this.chargeData[informationIndex].currency,
-              amount:       this.chargeData[informationIndex].amount,
+              chargeId: this.chargeData[informationIndex].id,
+              name: this.chargeData[informationIndex].name,
+              chargeTimeType: this.chargeData[informationIndex].chargeTimeType,
+              chargeCalculationType: this.chargeData[informationIndex].chargeCalculationType,
+              currency: this.chargeData[informationIndex].currency,
+              amount: this.chargeData[informationIndex].amount,
               amountPaid: 0,
               amountWaived: 0,
               amountWrittenOff: 0,
-              amountOutstanding:       this.chargeData[informationIndex].amount,
-              penalty:       this.chargeData[informationIndex].penalty,
-              chargePaymentMode:       this.chargeData[informationIndex].chargePaymentMode,
+              amountOutstanding: this.chargeData[informationIndex].amount,
+              penalty: this.chargeData[informationIndex].penalty,
+              chargePaymentMode: this.chargeData[informationIndex].chargePaymentMode,
               paid: false,
               waived: false,
               chargePayable: false,
-              id:       this.chargeData[informationIndex].id,
+              id: this.chargeData[informationIndex].id,
               isEndorsed: false,
-              insuranceName:       this.chargeData[informationIndex].chargeInsuranceDetailData?.insuranceName || "",
-              insuranceId:       this.chargeData[informationIndex].chargeInsuranceDetailData?.insuranceCode || ""
+              insuranceName: this.chargeData[informationIndex].chargeInsuranceDetailData?.insuranceName || '',
+              insuranceId: this.chargeData[informationIndex].chargeInsuranceDetailData?.insuranceCode || ''
             };
-      
+
             // Add charge to the chargesDataSource
             this.chargesDataSource = [...this.chargesDataSource, mappedItem];
           }
@@ -166,15 +173,27 @@ export class LoansAccountChargesStepComponent implements OnInit, OnChanges {
         this.overDueChargesDataSource = this.loansAccountProductTemplate.overdueCharges;
       }
       if (this.loansAccountProductTemplate.charges) {
-        if(this.loansAccountProductTemplate?.product?.productType?.name === "SU+ Vehiculos"){
+        if (this.loansAccountProductTemplate?.product?.productType?.name === 'SU+ Vehiculos') {
           this.isVehiculos = true;
         }
-        if(this.loanId){
-          console.log(this.loansAccountTemplate.charges)
-          this.chargesDataSource = this.loansAccountTemplate.charges.map((charge: any) => ({ ...charge, id: charge.chargeId, expdate: charge?.expDate, isEndorsed: charge.amount === 0, insuranceName: charge?.insuranceName, insuranceId : charge?.insuranceId  })) || [];
-        }else{
-          console.log("non ",this.loansAccountTemplate.charges)
-          this.chargesDataSource = this.loansAccountProductTemplate.charges.map((charge: any) => ({ ...charge, id: charge.chargeId, expdate: null, isEndorsed: charge.amount === 0, insuranceName: "", insuranceId : ""  })) || [];
+        if (this.loanId) {
+          this.chargesDataSource = this.loansAccountTemplate.charges.map((charge: any) => ({
+            ...charge,
+            id: charge.chargeId,
+            expdate: charge?.expDate,
+            isEndorsed: charge.amount === 0,
+            insuranceName: charge?.insuranceName,
+            insuranceId: charge?.insuranceId
+          })) || [];
+        } else {
+          this.chargesDataSource = this.loansAccountProductTemplate.charges.map((charge: any) => ({
+            ...charge,
+            id: charge.chargeId,
+            expdate: null,
+            isEndorsed: charge.amount === 0,
+            insuranceName: '',
+            insuranceId: ''
+          })) || [];
         }
 
       }
@@ -198,34 +217,34 @@ export class LoansAccountChargesStepComponent implements OnInit, OnChanges {
     const tomorrow = new Date(Date.now() + 86400000);
     this.maxDate = this.settingsService.maxFutureDate;
 
-    var formfields: FormfieldBase[];
+    let formfields: FormfieldBase[];
 
-    if(this.isVehiculos === true){
-       formfields = [
+    if (this.isVehiculos === true) {
+      formfields = [
         new InputBase(
           {
-          controlName: 'amount',
-          label: this.translate.instant('labels.inputs.Amount'),
-          value: charge.amount,
-          type: 'number',
-          required: false
-        }),
+            controlName: 'amount',
+            label: this.translate.instant('labels.inputs.Amount'),
+            value: charge.amount,
+            type: 'number',
+            required: false
+          }),
         new InputBase(
           {
-          controlName: 'insuranceName',
-          label: this.translate.instant('labels.inputs.insuranceName'),
-          value: charge?.insuranceName,
-          type: 'string',
-          required: false
-        }),
+            controlName: 'insuranceName',
+            label: this.translate.instant('labels.inputs.insuranceName'),
+            value: charge?.insuranceName,
+            type: 'string',
+            required: false
+          }),
         new InputBase(
           {
-          controlName: 'insuranceId',
-          label: this.translate.instant('labels.inputs.insuranceID'),
-          value: charge?.insuranceId,
-          type: 'string',
-          required: false
-        }),
+            controlName: 'insuranceId',
+            label: this.translate.instant('labels.inputs.insuranceID'),
+            value: charge?.insuranceId,
+            type: 'string',
+            required: false
+          }),
         new DatepickerBase({
           controlName: 'expdate',
           label: this.translate.instant('labels.inputs.expdate'),
@@ -236,42 +255,42 @@ export class LoansAccountChargesStepComponent implements OnInit, OnChanges {
           minDate: tomorrow
         })
       ];
-    }else{
-     formfields = [
-      new InputBase({
-        controlName: 'amount',
-        label: 'Amount',
-        value: charge.amount,
-        type: 'number',
-        required: false
-      }),
-    ];
-  }
+    } else {
+      formfields = [
+        new InputBase({
+          controlName: 'amount',
+          label: 'Amount',
+          value: charge.amount,
+          type: 'number',
+          required: false
+        }),
+      ];
+    }
     const data = {
       title: 'Edit Charge Amount',
-      layout: { addButtonText: 'Confirm' },
+      layout: {addButtonText: 'Confirm'},
       formfields: formfields
     };
-    const editNoteDialogRef = this.dialog.open(FormDialogComponent, { data });
+    const editNoteDialogRef = this.dialog.open(FormDialogComponent, {data});
     editNoteDialogRef.afterClosed().subscribe((response: any) => {
       if (response.data) {
-        var newCharge: any;
+        let newCharge: any;
         if (this.isVehiculos === true) {
-          var isEndorsed = false;
-          if (response.data.value.amount == 0) {
+          let isEndorsed = false;
+          if (response.data.value.amount === 0) {
             isEndorsed = true;
           }
-          let expdate = this.dateUtils.formatDate(response.data.value.expdate, "yyyy-MM-dd");
+          const expdate = this.dateUtils.formatDate(response.data.value.expdate, 'yyyy-MM-dd');
           newCharge = {
             ...charge,
             amount: response.data.value.amount,
             expdate: expdate,
             isEndorsed: isEndorsed,
             insuranceName: response.data.value.insuranceName,
-            insuranceId : response.data.value.insuranceId,
+            insuranceId: response.data.value.insuranceId,
           };
         } else {
-          newCharge = { ...charge, amount: response.data.value.amount };
+          newCharge = {...charge, amount: response.data.value.amount};
         }
         this.chargesDataSource.splice(this.chargesDataSource.indexOf(charge), 1, newCharge);
         this.chargesDataSource = this.chargesDataSource.concat([]);
@@ -296,10 +315,10 @@ export class LoansAccountChargesStepComponent implements OnInit, OnChanges {
     ];
     const data = {
       title: 'Edit Charge Date',
-      layout: { addButtonText: 'Confirm' },
+      layout: {addButtonText: 'Confirm'},
       formfields: formfields
     };
-    const editNoteDialogRef = this.dialog.open(FormDialogComponent, { data });
+    const editNoteDialogRef = this.dialog.open(FormDialogComponent, {data});
     editNoteDialogRef.afterClosed().subscribe((response: any) => {
       if (response.data) {
         let newCharge: any;
@@ -308,10 +327,10 @@ export class LoansAccountChargesStepComponent implements OnInit, OnChanges {
         switch (charge.chargeTimeType.value) {
           case 'Specified due date':
           case 'Weekly Fee':
-            newCharge = { ...charge, dueDate: date };
+            newCharge = {...charge, dueDate: date};
             break;
           case 'Annual Fee':
-            newCharge = { ...charge, feeOnMonthDay: date };
+            newCharge = {...charge, feeOnMonthDay: date};
             break;
         }
         this.chargesDataSource.splice(this.chargesDataSource.indexOf(charge), 1, newCharge);
@@ -337,13 +356,13 @@ export class LoansAccountChargesStepComponent implements OnInit, OnChanges {
     ];
     const data = {
       title: 'Edit Charge Fee Interval',
-      layout: { addButtonText: 'Confirm' },
+      layout: {addButtonText: 'Confirm'},
       formfields: formfields
     };
-    const editNoteDialogRef = this.dialog.open(FormDialogComponent, { data });
+    const editNoteDialogRef = this.dialog.open(FormDialogComponent, {data});
     editNoteDialogRef.afterClosed().subscribe((response: any) => {
       if (response.data) {
-        const newCharge = { ...charge, feeInterval: response.data.value.feeInterval };
+        const newCharge = {...charge, feeInterval: response.data.value.feeInterval};
         this.chargesDataSource.splice(this.chargesDataSource.indexOf(charge), 1, newCharge);
         this.chargesDataSource = this.chargesDataSource.concat([]);
       }
@@ -357,7 +376,7 @@ export class LoansAccountChargesStepComponent implements OnInit, OnChanges {
    */
   deleteCharge(charge: any) {
     const deleteChargeDialogRef = this.dialog.open(DeleteDialogComponent, {
-      data: { deleteContext: `charge ${charge.name}` }
+      data: {deleteContext: `charge ${charge.name}`}
     });
     deleteChargeDialogRef.afterClosed().subscribe((response: any) => {
       if (response.delete) {
